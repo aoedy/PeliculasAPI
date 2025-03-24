4c66948a54d85474df13b874563ec4607e78f01a
aoedy <edyalvarez1211@gmail.com>
2025-03-24 18:22:54 -0500

    Agregar archivos de proyecto.

---
+++ PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using PeliculasAPI.Entidades;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/generos")]
+    public class GenerosController: ControllerBase
+    {
+        [HttpGet] //api/generos
+        [HttpGet("listado")] //api/generos/listado
+        [HttpGet("/listado-generos")] //listado-generos
+
+        public List<Genero> Get()
+        {
+            var repositorio = new RepositorioEnMemoria();
+            var generos = repositorio.ObtenerTodosLosGeneros();
+            return generos;
+        }
+
+        [HttpGet("{id:int}")] //api/generos/500
+        [OutputCache]
+        public async Task<ActionResult<Genero>> Get(int id)
+        {
+            var repositorio = new RepositorioEnMemoria();
+            var genero =  await  repositorio.ObtenerPorId(id);
+
+            if (genero is null)
+            {
+                return NotFound();
+            }
+            return genero;
+        }
+
+        [HttpGet("{nombre}")] //api/generos/Felipe
+        public async Task<Genero?> Get(string nombre)
+        {
+            var repositorio = new RepositorioEnMemoria();
+            var genero = await repositorio.ObtenerPorId(1);
+            return genero;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+
+        [HttpPut]
+        public void Put()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+    }
+}
---
+++ PeliculasAPI/Entidades/Genero.cs
@@ -0,0 +1,8 @@
+﻿namespace PeliculasAPI.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+    }
+}
